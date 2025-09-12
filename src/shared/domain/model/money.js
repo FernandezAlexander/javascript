e356312c4999b311364b4a8a9c8a5c03f6f321f0
Alexander <u202019498@upc.edu.pe>
2025-09-12 16:07:39 -0500

    feat(money): add money.js

--- src/shared/domain/model/money.js
+++ src/shared/domain/model/money.js
@@ -102,6 +102,7 @@ export class Money {
      * @returns {string} The string representation of the Money instance.
      */
     toString() {
+        
         return `${this.#amount.toFixed(2)} ${this.#currency.code}`;
         
     }
